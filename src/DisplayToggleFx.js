d86cf9da058f8e1ac618692862435a27c4940f43
Avishkar Autar <aautar@digital-radiation.com>
2018-07-07 19:35:05 -0400

    Initial commit

---
+++ src/DisplayToggleFx.js
@@ -0,0 +1,70 @@
+/**
+ * DisplayToggleFx helps with applying/unapplying CSS classes that trigger CSS transitions, 
+ * where the DOM element needs to change from a display:none state → displayed state, and vice-versa
+ */
+const DisplayToggleFx = {
+
+    /**
+     * @param {Element} _elem
+     * @param {String} _fxClasses
+     * @param {String} _cssDisplayVal
+     */
+    apply: function(_elem, _fxClasses, _cssDisplayVal) {
+        _elem.style.display = _cssDisplayVal || 'block';
+        _elem.offsetLeft;
+
+        for(let i=0; i<_fxClasses.length; i++) {
+            _elem.classList.add(_fxClasses[i]);
+        }
+    },
+
+    /**
+     * @param {Element} _elem
+     * @returns {Number}
+     */
+    getMaxTransitionDuration: function(_elem) {
+        const allTransitionDurations = window.getComputedStyle(_elem).getPropertyValue('transition-duration');
+        allTransitionDurations.split(',');
+        
+        const maxDurationMs = 0;
+
+        allTransitionDurations.forEach(function(_dur) {
+            if(_dur.indexOf('s') !== -1) {
+                const durationMs = (_dur.replace(/s/g, '')) * 1000.0;
+                if(durationMs > maxDurationMs) {
+                    maxDurationMs = durationMs;
+                }
+                return;
+            }
+
+            if(_dur.indexOf('ms') !== -1) {
+                const durationMs = (_dur.replace(/ms/g, ''));
+                if(durationMs > minDurationMs) {
+                    maxDurationMs = durationMs;
+                }
+                return;
+            }
+        });
+
+        return maxDurationMs;
+    },
+
+    /**
+     * @param {Element} _elem
+     * @param {String} _fxClasses
+     */
+    unapply: function(_elem, _fxClasses) {
+
+        const maxTransitionDuration = Play.getMaxTransitionDuration(_elem);
+
+        for(let i=0; i<_fxClasses.length; i++) {       
+            _elem.classList.remove(_fxClasses[i]);
+        }
+
+        setTimeout(function() {
+            _elem.style.display = "none";
+        }, maxTransitionDuration);
+    }
+};
+
+export { DisplayToggleFx }
